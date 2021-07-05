ca12757d05170d61adea165439d2ed011d2ab937
rawfoon <86964117+rawfoon@users.noreply.github.com>
2021-07-05 18:27:40 +0600

    all uploaded

---
+++ main.js
@@ -0,0 +1,48 @@
+const nextIcon = '<img src="../img/right-arrow.svg">';
+const prevIcon = '<img src="../img/previous.svg">';
+
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        margin: 15,
+        items: 5,
+        loop: true,
+        nav: true,
+        navText: [
+            prevIcon,
+            nextIcon
+        ],
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 3500,
+        responsiveClass: true,
+        responsiveRefreshRate: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            991: {
+                items: 4
+            },
+            1200: {
+                items: 4
+            },
+            1920: {
+                items: 5
+            },
+
+        }
+
+    });
+});
+
+
+
+$(function() {
+    $('#menu').slicknav();
+});
